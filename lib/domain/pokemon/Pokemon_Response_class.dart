a72472b68cd172b0ac5a6baa23d5058add65a96c
Lucas Carvalho <49245496+LucssCarvalho@users.noreply.github.com>
2020-04-21 23:36:36 -0300

    update PokemonResponse

--- lib/domain/pokemon/Pokemon_Response_class.dart
+++ lib/domain/pokemon/Pokemon_Response_class.dart
@@ -6,18 +6,13 @@ import 'package:pokedex_flutter/domain/pokemon/Sprites_Response_class.dart';
 import 'package:pokedex_flutter/domain/pokemon/Stats_Response_class.dart';
 import 'package:pokedex_flutter/domain/pokemon/TypesResponse_class.dart';
 
-import 'package:json_annotation/json_annotation.dart';
-
-// part 'pokemon_response_class.g.dart';
-
-@JsonSerializable(explicitToJson: true, fieldRename: FieldRename.snake)
 class PokemonResponse {
   List<Abilities> abilities;
   int baseExperience;
   // List<Forms> forms;
   List<GameIndices> gameIndices;
   int height;
-  List<Null> heldItems;
+  // List<Null> heldItems;
   int id;
   bool isDefault;
   String locationAreaEncounters;
@@ -36,7 +31,6 @@ class PokemonResponse {
       // this.forms,
       this.gameIndices,
       this.height,
-      this.heldItems,
       this.id,
       this.isDefault,
       this.locationAreaEncounters,
@@ -48,4 +42,86 @@ class PokemonResponse {
       this.stats,
       this.types,
       this.weight});
+
+  PokemonResponse.fromJson(Map<String, dynamic> json) {
+    if (json['abilities'] != null) {
+      abilities = new List<Abilities>();
+      json['abilities'].forEach((v) {
+        abilities.add(new Abilities.fromJson(v));
+      });
+    }
+    baseExperience = json['base_experience'];
+    if (json['game_indices'] != null) {
+      gameIndices = new List<GameIndices>();
+      json['game_indices'].forEach((v) {
+        gameIndices.add(new GameIndices.fromJson(v));
+      });
+    }
+    height = json['height'];
+
+    id = json['id'];
+    isDefault = json['is_default'];
+    locationAreaEncounters = json['location_area_encounters'];
+    if (json['moves'] != null) {
+      moves = new List<Moves>();
+      json['moves'].forEach((v) {
+        moves.add(new Moves.fromJson(v));
+      });
+    }
+    name = json['name'];
+    order = json['order'];
+    species =
+        json['species'] != null ? new Ability.fromJson(json['species']) : null;
+    sprites =
+        json['sprites'] != null ? new Sprites.fromJson(json['sprites']) : null;
+    if (json['stats'] != null) {
+      stats = new List<Stats>();
+      json['stats'].forEach((v) {
+        stats.add(new Stats.fromJson(v));
+      });
+    }
+    if (json['types'] != null) {
+      types = new List<Types>();
+      json['types'].forEach((v) {
+        types.add(new Types.fromJson(v));
+      });
+    }
+    weight = json['weight'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    if (this.abilities != null) {
+      data['abilities'] = this.abilities.map((v) => v.toJson()).toList();
+    }
+    data['base_experience'] = this.baseExperience;
+
+    if (this.gameIndices != null) {
+      data['game_indices'] = this.gameIndices.map((v) => v.toJson()).toList();
+    }
+    data['height'] = this.height;
+
+    data['id'] = this.id;
+    data['is_default'] = this.isDefault;
+    data['location_area_encounters'] = this.locationAreaEncounters;
+    if (this.moves != null) {
+      data['moves'] = this.moves.map((v) => v.toJson()).toList();
+    }
+    data['name'] = this.name;
+    data['order'] = this.order;
+    if (this.species != null) {
+      data['species'] = this.species.toJson();
+    }
+    if (this.sprites != null) {
+      data['sprites'] = this.sprites.toJson();
+    }
+    if (this.stats != null) {
+      data['stats'] = this.stats.map((v) => v.toJson()).toList();
+    }
+    if (this.types != null) {
+      data['types'] = this.types.map((v) => v.toJson()).toList();
+    }
+    data['weight'] = this.weight;
+    return data;
+  }
 }
