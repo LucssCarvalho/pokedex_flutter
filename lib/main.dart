8a5436b29859b7a29f6bc35484ac3f036b3c243f
Lucas Carvalho <49245496+LucssCarvalho@users.noreply.github.com>
2021-04-25 18:08:13 -0300

    remove title

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,10 @@ import 'package:pokedex_flutter/screen/pokedexScreen.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
